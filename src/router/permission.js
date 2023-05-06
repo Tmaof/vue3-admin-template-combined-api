1e5a08f4d2ec3dcb9e36c79b33fcba3c7fca5764
MAOFU <2148334528@qq.com>
2023-05-06 14:29:45 +0800

    fix(custom): 修改导航守卫中的过期判断方法
    const ok = await store.dispatch('user/getUserInfo')

--- src/router/permission.js
+++ src/router/permission.js
@@ -5,16 +5,24 @@ import setDocumentTile from '@/utility/set-document-title'
 const WhiteRoute = ['/login', '/register']
 
 export function useBeforeEach(router) {
-  router.beforeEach((to, from, next) => {
+  router.beforeEach(async (to, from, next) => {
     // 设置文章标题
     if (to.meta && to.meta.title) {
       setDocumentTile(to.meta.title, true)
     }
     // 不需要权限的页面
     if (WhiteRoute.includes(to.path)) {
-      // 有token，不能再次进入登录页
+      // 一般来说,有token，不能再次进入登录页
+      // 但是,有token,而没有任何私有页面权限的用户可以返回登录注册页
       if (['/login', '/register'].includes(to.path) && store.getters.token) {
-        next(from.path)
+        if (!store.getters.userInfo) {
+          await store.dispatch('user/getUserInfo')
+        }
+        if (store.getters.userInfo.permission.menus.length === 0) {
+          next()
+        } else {
+          next(from.path)
+        }
       } else {
         next()
       }
@@ -33,19 +41,19 @@ export function useBeforeEach(router) {
         // 没有用户信息，代表用户刚刚访问本站
         else {
           // 用已有的token去获取用户信息，看token是否过期
-          store
-            .dispatch('user/getUserInfo')
-            .then(() => {
-              // 没有过期，放行
-              next(to.fullPath)
-            })
-            .catch(() => {
-              // token过期，需要用户重新登录
-              // 先清除token，才能跳转登录页
-              ElMessage.warning('用户认证已经过期，请重新登录！谢谢')
-              store.commit('user/SET_token', '')
-              next('/login')
-            })
+          const ok = await store.dispatch('user/getUserInfo')
+
+          if (ok) {
+            // 没有过期，放行
+            // 由于在导航守卫中添加了动态路由,所以需要重定向,而不是直接next()
+            next(to.fullPath)
+          } else {
+            // token过期，需要用户重新登录
+            // 先清除token，才能跳转登录页
+            ElMessage.warning('用户认证已经过期，请重新登录！谢谢')
+            store.commit('user/SET_token', '')
+            next('/login')
+          }
         }
         // eslint-disable-next-line brace-style
       }
